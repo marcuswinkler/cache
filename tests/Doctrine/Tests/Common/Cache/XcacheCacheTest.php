3a312feaa9c6375c72795e5ef8a7f235af5b7781
Marcus Winkler <marcus.winkler@gmx.de>
2013-11-08 10:22:41 +0100

    Skip xcache tests if xcache unavailable

--- tests/Doctrine/Tests/Common/Cache/XcacheCacheTest.php
+++ tests/Doctrine/Tests/Common/Cache/XcacheCacheTest.php
@@ -11,6 +11,12 @@ class XcacheCacheTest extends CacheTest
         if ( ! extension_loaded('xcache')) {
             $this->markTestSkipped('The ' . __CLASS__ .' requires the use of xcache');
         }
+        if (php_sapi_name() == 'cli' && version_compare(XCACHE_VERSION, '3.1.0', 'lt')) {
+            $this->markTestSkipped('The ' . __CLASS__ .' cannot be tested on the command line with xcache versions before 3.1.0');
+        }
+        if (ini_get('xcache.var_size') <= 0) {
+            $this->markTestSkipped('The ' . __CLASS__ .' requires php config option "xcache.var_size" to be > 0');
+        }
     }
 
     protected function _getCacheDriver()
